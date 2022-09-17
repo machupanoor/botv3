666f288dceaae202f545777fa9f0b5136d88ec9b
machupanoor <113260055+machupanoor@users.noreply.github.com>
2022-09-17 20:08:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -72,20 +72,20 @@ async def start(client, message):
             ]
         ]
 
-#         if message.command[1] != "subscribe":
-#             try:
-#                 kk, file_id = message.command[1].split("_", 1)
-#                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
-#                 btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
-#             except (IndexError, ValueError):
-#                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
-#         await client.send_message(
-#             chat_id=message.from_user.id,
-#             text="**Please Join My Updates Channel to use this Bot!**",
-#             reply_markup=InlineKeyboardMarkup(btn),
-#             parse_mode=enums.ParseMode.MARKDOWN
-#             )
-#         return
+        if message.command[1] != "subscribe":
+            try:
+                kk, file_id = message.command[1].split("_", 1)
+                pre = 'checksubp' if kk == 'filep' else 'checksub' 
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
+            except (IndexError, ValueError):
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+        await client.send_message(
+            chat_id=message.from_user.id,
+            text="**Please Join My Updates Channel to use this Bot!**",
+            reply_markup=InlineKeyboardMarkup(btn),
+            parse_mode=enums.ParseMode.MARKDOWN
+            )
+        return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
